0945bc3e78a27da876c8ffd917d7a6d8be2c19b9
Julian Knauer <jpk@goatpr0n.de>
2012-03-21 16:04:43 +0100

    GPLv2 added

--- g35d.c
+++ g35d.c
@@ -1,3 +1,22 @@
+/*
+ *  G35 Daemon for Linux
+ *  Copyright (C) 2012  Julian Knauer <jpk-at-goatpr0n.de>
+ *
+ *  This program is free software; you can redistribute it and/or
+ *  modify it under the terms of the GNU General Public License
+ *  as published by the Free Software Foundation; either version 2
+ *  of the License, or (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program; if not, write to the Free Software
+ *  Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ */
+
 #define _GNU_SOURCE
 #include <stdio.h>
 #include <stdlib.h>
